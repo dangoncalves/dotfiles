b7906465591d975ef944e3c0413721f42c5cca65
Daniel Gonçalves <daniel.gonc@lves.fr>
2021-08-12 06:34:31 +0200

    [fish] - Change tmux session name

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     and not set -q TMUX
     tmux start-server
-    test -z $DISPLAY; and set TMUX_SESSION_NAME default-nox; or set TMUX_SESSION_NAME default
+    test -z $DISPLAY; and set TMUX_SESSION_NAME (hostname)-nox; or set TMUX_SESSION_NAME (hostname)
     test (tmux ls 2> /dev/null | grep $TMUX_SESSION_NAME | wc -l) -eq 0
     and tmux new-session -s $TMUX_SESSION_NAME
     or tmux new-session -t $TMUX_SESSION_NAME
