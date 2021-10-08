7816024e6dc2ce674ba6b27a72c1991d72efe0a5
Daniel Gonçalves <daniel.gonc@lves.fr>
2021-10-08 19:06:49 +0200

    [fish] - fix is-interactive bug

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,22 @@
 if status is-interactive
-    and not set -q TMUX
-    tmux start-server
-    test -z $DISPLAY; and set TMUX_SESSION_NAME (hostname)-nox; or set TMUX_SESSION_NAME (hostname)
-    test (tmux ls 2> /dev/null | grep $TMUX_SESSION_NAME | wc -l) -eq 0
-    and tmux new-session -s $TMUX_SESSION_NAME
-    or tmux new-session -t $TMUX_SESSION_NAME
-end
+    if not set -q TMUX
+        tmux start-server
+        test -z $DISPLAY; and set TMUX_SESSION_NAME (hostname)-nox; or set TMUX_SESSION_NAME (hostname)
+        test (tmux ls 2> /dev/null | grep $TMUX_SESSION_NAME | wc -l) -eq 0
+        and tmux new-session -s $TMUX_SESSION_NAME
+        or tmux new-session -t $TMUX_SESSION_NAME
+    end
 
-set -x VAGRANT_DEFAULT_PROVIDER libvirt
-set -x LIBVIRT_DEFAULT_URI quemu:///system
+    set -x VAGRANT_DEFAULT_PROVIDER libvirt
+    set -x LIBVIRT_DEFAULT_URI quemu:///system
 
-source ~/.config/fish/aliases.fish
+    set -x GPG_TTY (tty)
+    set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+    gpgconf --launch gpg-agent
 
-if test -f utils.fish
-    source utils.fish
+    source ~/.config/fish/aliases.fish
+
+    if test -f utils.fish
+        source utils.fish
+    end
 end
